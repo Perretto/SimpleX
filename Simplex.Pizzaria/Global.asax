f71ea0dc6d11fb83ccb36017ab9f66ba6bb7f0e3
Paulo Hidenari Uieti <paulohidenari@hotmail.com>
2016-02-11 01:51:36 -0200

    Criacao do projeto mvc pizzaria

---
+++ Simplex.Pizzaria/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Simplex.Pizzaria.MvcApplication" Language="C#" %>
---
+++ Simplex.Pizzaria/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Simplex.Pizzaria.Startup))]
+namespace Simplex.Pizzaria
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
