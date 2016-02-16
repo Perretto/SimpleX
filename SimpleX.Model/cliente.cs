69696a86728dbc7dfa212843789cf8c98bd88dfb
RepositorySimplex <barra1985@hotmail.com>
2016-02-16 18:29:38 -0200

    Modelo de cliente

---
+++ SimpleX.Model/cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleX.Model
+{
+    public class cliente
+    {
+        public Guid ID { get; set; }
+        public string razaoSocial { get; set; }
+        public string nomeFantasia { get; set; }
+        public string CNPJ { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string IE { get; set; }
+        public string IM { get; set; }
+        public Guid CNAEID { get; set; }
+        public string suframa { get; set; }
+        public Guid empresaID { get; set; } 
+
+    }
+}
