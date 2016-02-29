3fd443155dc85d9b48e44cc1179090dfc2c38db9
RepositorySimplex <barra1985@hotmail.com>
2016-02-29 19:13:35 -0300

    ajuste area

---
+++ Simplex.Pizzaria/Areas/Cliente/Controllers/ClienteController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simplex.Pizzaria.Areas.Cliente.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente/Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ClienteListagem()
+        {
+
+            return View();
+        }
+        
+    }
+}
