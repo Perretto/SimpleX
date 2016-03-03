d69f6975efdcfd5bbf181b020f06e34056af6aaa
RepositorySimplex <barra1985@hotmail.com>
2016-03-03 15:26:58 -0300

    listagem produto

---
+++ Simplex.Pizzaria/Areas/Produto/ProdutoAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Simplex.Pizzaria.Areas.Produto
+{
+    public class ProdutoAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Produto";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Produto_default",
+                "Produto/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
