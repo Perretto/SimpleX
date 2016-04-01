7df7a5000d8de25018b86f1f1ed3a18efb5d9744
RepositorySimplex <barra1985@hotmail.com>
2016-04-01 13:04:42 -0300

    Cadastro de usuario

---
+++ Simplex.Pizzaria/Areas/Administrador/Controllers/AdministradorController.cs
@@ -0,0 +1,89 @@
+﻿using SimpleX.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleX.Core.Facade;
+using SimpleX.ModelCore;
+
+namespace Simplex.Pizzaria.Areas.Administrador.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        administracaoFacade facadeAdministrador;
+        cadastroFacade facadeCadastro;
+
+        public AdministradorController()
+        {
+            facadeAdministrador = new administracaoFacade();
+            facadeCadastro = new cadastroFacade();
+        }
+
+        // GET: Administrador/Administrador
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult usuarioListagem()
+        {
+            usuario usuario = new usuario();
+            usuario.empresaID = Guid.Parse("fc70ecab-61b8-4e53-9a99-6098b0a75a02");
+            List<usuario> lstUsuario = facadeAdministrador.FiltrarUsuario(usuario);
+            
+
+            return View(lstUsuario);
+        }
+
+        public ActionResult usuarioCadastroEdicao(string idUsuario = "")
+        {
+            usuario usuario = new usuario();
+            if (idUsuario != "")
+            {
+                usuario = facadeAdministrador.ConsultarUsuario(Guid.Parse(idUsuario));
+            }
+
+            return View("usuarioCadastro", usuario);
+        }
+
+        public ActionResult usuarioCadastro(string idUsuario = "")
+        {
+            usuario usuario = new usuario();
+            if (idUsuario != "")
+            {
+                usuario = facadeAdministrador.ConsultarUsuario(Guid.Parse(idUsuario));
+            }
+
+            return View("usuarioCadastro", usuario);
+        }
+
+        //Salvar============================================================================================
+        [HttpPost]
+        public ActionResult SalvarUsuario(usuario usuario)
+        {
+            usuario.empresaID = Guid.Parse("fc70ecab-61b8-4e53-9a99-6098b0a75a02");
+            Result resultado = facadeAdministrador.SalvarUsuario(usuario);
+            if (usuario.ID != Guid.Empty)
+            {
+                resultado.AddMensagem("ID", usuario.ID.ToString());
+                resultado.Sucesso = true;
+            }
+
+            return Json(resultado);
+        }
+
+        //Excluir============================================================================================
+        public ActionResult ExcluirUsuario(string idUsuario = "")
+        {
+            Result resultado = new Result();
+
+            if (idUsuario != "")
+            {
+                resultado = facadeAdministrador.ExcluirUsuario(Guid.Parse(idUsuario));
+            }
+            return Json(resultado, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Simplex.Pizzaria/Scripts/framework/jquery.simplexis.usuario.js
@@ -0,0 +1,55 @@
+﻿function salvarUsuario() {
+    var id = "form-usuario";
+
+    var parameters = new Object();
+    parameters.dataType = "";
+    parameters.type = "POST";
+    parameters.url = "http://" + window.location.host + "/Administrador/Administrador/salvarUsuario";
+    parameters.callback = "";
+    parameters.async = false;
+    //parameters.type = "application/json; charset=utf-8";
+    parameters.dados = $("#" + id).serialize();
+
+    parameters.callback = function (result) {
+        if (result.Sucesso == true) {
+            notification({ messageTitle: "OK", messageText: result.MensagemGeral, fix: false, type: "ok", icon: "thumbs-up" });
+
+            if (result.Campos.length > 0) {
+                var idProduto = result.Campos[0].Mensagem;
+                window.location = "http://" + window.location.host + "/Administrador/Administrador/usuarioListagem";
+            } else {
+                location.reload();
+            }
+        } else {
+            notification({ messageTitle: "Ops", messageText: result.MensagemGeral, fix: false, type: "Ops", icon: "thumbs-down" });
+        }
+    };
+
+    AjaxParameter(parameters);
+
+}
+
+function cancelarUsuario() {
+    window.location = "http://" + window.location.host + "/Administrador/Administrador/usuarioListagem";
+}
+
+function excluirUsuario(idUsuario) {
+    var parameters = new Object();
+    parameters.dataType = "";
+    parameters.type = "get";
+    parameters.url = "http://" + window.location.host + "/Administrador/Administrador/ExcluirUsuario?idUsuario=" + idUsuario;
+    parameters.callback = "";
+    parameters.async = false;
+    //parameters.type = "application/json; charset=utf-8";
+    parameters.callback = function (result) {
+        if (result.Sucesso == true) {
+            notification({ messageTitle: "OK", messageText: result.MensagemGeral, fix: false, type: "ok", icon: "thumbs-up" });
+
+            location.reload();
+        } else {
+            notification({ messageTitle: "Ops", messageText: result.MensagemGeral, fix: false, type: "Ops", icon: "thumbs-down" });
+        }
+    };
+
+    AjaxParameter(parameters);
+}
