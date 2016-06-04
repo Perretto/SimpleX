a2c5a582a2e74342badef8134f1379ef9015919e
RepositorySimplex <barra1985@hotmail.com>
2016-06-04 11:52:36 -0300

    models

---
+++ Simplex.Pizzaria/Service/fornecedorService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Simplex.Pizzaria.Service
+{
+    public class fornecedorService
+    {
+    }
+}
