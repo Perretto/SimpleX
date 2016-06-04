50598dd69fe08570e813febb6b9da0f30d4f131f
RepositorySimplex <barra1985@hotmail.com>
2016-06-04 11:33:41 -0300

    ajuste genérico

---
+++ Simplex.Pizzaria/Models/CFOP.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using SimpleX.Model;
+
+namespace Simplex.Pizzaria.Models
+{
+    public class CFOP : SimpleX.Model.CFOP
+    {
+       
+    }
+}
---
+++ Simplex.Pizzaria/Models/fornecedorContato.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Simplex.Pizzaria.Models
+{
+    public class fornecedorContato : SimpleX.Model.fornecedorContato
+    {
+        
+    }
+}
