d072306f495e5e575cc5e15abe73321ec8451491
RepositorySimplex <barra1985@hotmail.com>
2016-06-04 10:27:48 -0300

    mensagem de sistema

---
+++ SimpleX.Model/systemMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleX.Model
+{
+    public class systemMessage
+    {
+        public Guid ID { get; set; } 
+        public string type { get; set; }
+        public string internalNumber { get; set; }
+        public string externalNumber { get; set; }
+        public string description { get; set; }
+        public bool success { get; set; }
+    }
+}
