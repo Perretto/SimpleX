342bd4c96d716950f09a9a8ad135b430d06ac612
RepositorySimplex <barra1985@hotmail.com>
2016-06-04 10:32:24 -0300

    ajustes nos modelos

---
+++ Simplex.Pizzaria/Service/clienteService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using SimpleX.ModelCore;
+using Simplex.Pizzaria.Repository;
+using SimpleX.Model;
+using System.Data.Entity;
+using Simplex.Pizzaria.Context;
+using SimpleX.ModelCore.Message;
+
+namespace Simplex.Pizzaria.Service
+{
+
+    public class clienteService <T> : pizzariaService<T> where T : class
+    {
+        private ContextPizzaria context;
+        private Repositorio<cliente> repositoryCliente;
+
+        public clienteService()
+        {
+            context = new ContextPizzaria();
+            repositoryCliente = new Repositorio<cliente>(context);
+        }
+        
+                
+        public List<cliente> Filtrar(cliente cliente)
+        {
+            return repositoryCliente.ObterPorFiltros(b => (
+                (cliente.ID == Guid.Empty || b.ID == cliente.ID) &&
+                (cliente.razaoSocial == null || b.razaoSocial.ToUpper().Contains(cliente.razaoSocial)) &&
+                (cliente.nomeFantasia == null || b.nomeFantasia.ToUpper().Contains(cliente.nomeFantasia)) &&
+                (cliente.codigo == null || b.codigo == cliente.codigo) &&
+                (cliente.CNPJ == null || b.CNPJ.ToUpper().Contains(cliente.CNPJ)) &&
+                (cliente.CPF == null || b.CPF.ToUpper().Contains(cliente.CPF)) &&
+                (cliente.RG == null || b.RG.ToUpper().Contains(cliente.RG)) &&
+                (cliente.IE == null || b.IE.ToUpper().Contains(cliente.IE)) &&
+                (cliente.IM == null || b.IM.ToUpper().Contains(cliente.IM)) &&
+                (cliente.suframa == null || b.suframa.ToUpper().Contains(cliente.suframa)) &&
+                (cliente.CNAEID == Guid.Empty || b.CNAEID == cliente.CNAEID) &&                
+                (cliente.empresaID == Guid.Empty || b.empresaID == cliente.empresaID)
+                )).ToList();
+        }
+
+    }
+}
---
+++ Simplex.Pizzaria/Service/vendaProdutoService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using SimpleX.ModelCore;
+using Simplex.Pizzaria.Repository;
+using System.Data.Entity;
+using Simplex.Pizzaria.Context;
+using SimpleX.ModelCore.Message;
+using Simplex.Pizzaria.Models;
+
+namespace Simplex.Pizzaria.Service
+{
+
+    public class vendaProdutoService<T> : pizzariaService<T> where T : class
+    {
+        private ContextPizzaria context;
+        private Repositorio<vendaProduto> repositoryVendaProduto;
+
+        public vendaProdutoService()
+        {
+            context = new ContextPizzaria();
+            repositoryVendaProduto = new Repositorio<vendaProduto>(context);
+        }
+
+
+        public List<vendaProduto> Filtrar(Simplex.Pizzaria.Models.vendaProduto vendaProduto)
+        {
+            return repositoryVendaProduto.ObterPorFiltros(b => (
+                (vendaProduto.ID == Guid.Empty || b.ID == vendaProduto.ID) &&
+                (vendaProduto.vendaID == Guid.Empty || b.vendaID == vendaProduto.vendaID) &&
+                (vendaProduto.produtoID == Guid.Empty || b.produtoID == vendaProduto.produtoID) &&
+                (vendaProduto.subProdutoID == Guid.Empty || b.subProdutoID == vendaProduto.subProdutoID) &&
+                (vendaProduto.valorUnitario == 0 || b.valorUnitario == vendaProduto.valorUnitario) &&
+                (vendaProduto.quantidade == 0 || b.quantidade == vendaProduto.quantidade) &&
+                (vendaProduto.valorTotal == 0 || b.valorTotal == vendaProduto.valorTotal) &&
+                (vendaProduto.empresaID == Guid.Empty || b.empresaID == vendaProduto.empresaID)
+                )).ToList();
+        }
+
+    }
+}
